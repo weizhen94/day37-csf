e742a5d90eb4035b752a8635db7475d6e3903b10
weizhen94 <weizhen94@gmail.com>
2023-05-03 14:43:20 +0800

    first commit

---
+++ server/day37-springboot/src/main/java/com/example/workshop37/Workshop37Application.java
@@ -0,0 +1,13 @@
+package com.example.workshop37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop37Application.class, args);
+	}
+
+}
---
+++ server/day37-springboot/src/test/java/com/example/workshop37/Workshop37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshop37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
